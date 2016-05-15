1d545fd1c5f1cb899a9981f71e8ee3eca0f71ce2
Sylvestre Gug <sylvestre@outriggermedia.com>
2016-05-15 17:38:06 -0400

    Full DB config_2

--- wdwe/settings.py
+++ wdwe/settings.py
@@ -88,8 +88,8 @@ if os.getenv('DATABASE_URL'):
 
     # Update database configuration with $DATABASE_URL.
     import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(os.getenv('DATABASE_URL'))
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
 
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
